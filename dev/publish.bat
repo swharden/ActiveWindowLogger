8adb93b248a5128b60ad4d10558000de6bfdb27d
Scott W Harden <swharden@gmail.com>
2025-07-29 15:08:34 -0400

    dev: update build script

--- dev/publish.bat
+++ dev/publish.bat
@@ -1,4 +1,4 @@
 cd ..\src\ActiveWindowLogger
-dotnet publish -c Release -r win-x64 -p:PublishSingleFile=true -p:IncludeNativeLibrariesForSelfExtract=false -o bin/publish
-explorer bin
+dotnet publish -c Release -r win-x64 -p:PublishSingleFile=true --self-contained false -p:DebugType=embedded -o bin/publish
+explorer bin\publish
 pause
